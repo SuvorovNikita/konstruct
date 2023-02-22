7bec8c6a5594990d2c1e9976e51451a0071c49f3
NS <nikit.suvorov@gmail.com>
2023-02-22 14:48:16 +0300

    maping component card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,39 +3,42 @@ import icon2 from "../../assets/img/icon-2.svg";
 import icon3 from "../../assets/img/icon-3.svg";
 
 const Card = () => {
+
+    const card = [
+        {
+            id: "1",
+            url: icon1,
+            alt: "icon-1.svg",
+            title: "Project and construction management",
+            text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat."
+        },
+        {
+            id: "2",
+            url: icon2,
+            alt: "icon-2.svg",
+            title: "Structure audit and maintenance",
+            text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat."
+        },
+        {
+            id: "3",
+            url: icon3,
+            alt: "icon-3.svg",
+            title: "Factory construction and modeling",
+            text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat."
+        },
+    ];
+
+
     return (
         <>
             <div className="c-card__inner">
-                <div className="c-card__body c-card__body--mr-28">
+                {card.map(el => <div className="c-card__body c-card__body--mr-28">
                     <div className="c-card__image">
-                        <img className="c-card__icon" src={icon1} alt="icon-1"/>
+                        <img className="c-card__icon" src={el.url} alt={el.alt}/>
                     </div>
-                    <h3 className="c-card__title">PROJECT AND CONSTRUCTION MANAGEMENT</h3>
-                    <p className="c-card__text">Lorem ipsum dolor sit amet,<br/> consectetuer adipiscing
-                        elit, sed diam <br/> nonummy nibh euismod tincidunt ut <br/> laoreet dolore magna aliquam
-                        erat.
-                    </p>
-                </div>
-                <div className="c-card__body c-card__body--mr-28">
-                    <div className="c-card__image">
-                        <img className="c-card__icon" src={icon2} alt="icon-1"/>
-                    </div>
-                    <h3 className="c-card__title">PROJECT AND CONSTRUCTION MANAGEMENT</h3>
-                    <p className="c-card__text">Lorem ipsum dolor sit amet,<br/> consectetuer adipiscing
-                        elit, sed diam <br/> nonummy nibh euismod tincidunt ut <br/> laoreet dolore magna aliquam
-                        erat.
-                    </p>
-                </div>
-                <div className="c-card__body">
-                    <div className="c-card__image">
-                        <img className="c-card__icon" src={icon3} alt="icon-1"/>
-                    </div>
-                    <h3 className="c-card__title">PROJECT AND CONSTRUCTION MANAGEMENT</h3>
-                    <p className="c-card__text">Lorem ipsum dolor sit amet,<br/> consectetuer adipiscing
-                        elit, sed diam <br/> nonummy nibh euismod tincidunt ut <br/> laoreet dolore magna aliquam
-                        erat.
-                    </p>
-                </div>
+                    <h3 className="c-card__title">{el.title}</h3>
+                    <p className="c-card__text">{el.text}</p>
+                </div>)}
             </div>
         </>
     )
